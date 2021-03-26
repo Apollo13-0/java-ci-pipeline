8b70e0f0056ee6a600fca6121f247ecaaebe925e
Apollo13-0 <calderon.ig08@estudiantec.cr>
2021-03-26 14:35:58 -0600

    finish

--- src/Calculator.java
+++ src/Calculator.java
@@ -18,7 +18,7 @@ public class Calculator {
     }
 
     public static void main(String[] args){
-
+        System.out.println("Funciona");
     }
 
 }
--- src/TestCalculator.java
+++ src/TestCalculator.java
@@ -1,6 +1,6 @@
 import junit.framework.TestCase;
 
-public class CalculatorTest extends TestCase {
+public class TestCalculator extends TestCase {
     private Calculator calculator;
 
     public void newCalculator (){
@@ -18,26 +18,32 @@ public class CalculatorTest extends TestCase {
     }
 
     public void testSubtraction1 (){
-        ;
+        newCalculator();
+        assertTrue(calculator.subtraction(4, 3) == (4 - 3));
     }
 
     public void testSubtraction2 (){
-        ;
+        newCalculator();
+        assertEquals(10, calculator.subtraction(22,12));
     }
 
     public void testMultiplication1 (){
-        ;
+        newCalculator();
+        assertTrue(calculator.multiplication(4, 4) == (4 * 4));
     }
 
     public void testMultiplication2 (){
-        ;
+        newCalculator();
+        assertEquals(8, calculator.multiplication(2,4));
     }
 
     public void testDivision1 (){
-        ;
+        newCalculator();
+        assertTrue(calculator.division(8, 2) == (8 / 2));
     }
 
     public void testDivision2 (){
-        ;
+        newCalculator();
+        assertEquals((9/3), calculator.division(9,3));
     }
 }
